584a63d7d75eaecc677f2311a181fd1d38a7a0fe
illia-fz <77698519+illia-fz@users.noreply.github.com>
2025-11-19 16:08:26 -0500

    Refactor: Minor code style improvement in compound.ts (Commit 2)

--- src/compound.ts
+++ src/compound.ts
@@ -4,3 +4,4 @@
 // Commit 4: Adding a comment to compound.ts
 // Commit 5: Adding a comment to compound.ts
 // Commit 1: Adding a comment to compound.ts
+// Commit 2: Adding a comment to compound.ts
